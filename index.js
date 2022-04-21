318bb1ba117711d5f928492da9d4b718ac4cb0e7
Estevam <estevamfurtado@gmail.com>
2022-04-20 23:25:56 -0300

    post signup & post+get tweets

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
 import express from 'express';
 import cors from 'cors';
+import chalk from 'chalk';
+import bodyParser from 'body-parser';
 
 const app = express();
+
+app.use(bodyParser.json());
 app.use(cors());
 
+const user = {};
+const tweets = [];
+
+app.post("/sign-up", (req, res) => {
+    const { username, avatar } = req.body;
+    user.username = username;
+    user.avatar = avatar;
+    res.sendStatus(200);
+})
+
+app.post("/tweets", (req, res) => {
+
+    const { tweet } = req.body;
+    tweets.push({
+        username: user.username,
+        avatar: user.avatar,
+        tweet: tweet
+    });
+    res.sendStatus(200);
+})
+
+app.get("/tweets", (req, res) => {
+    res.send(tweets);
+})
+
+app.listen(5000, () => console.log(`Started server at http://localhost:5000!`));
