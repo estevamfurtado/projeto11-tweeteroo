f1259185cdb8bf97be7f9f15047f6975c1cb1129
Estevam <estevamfurtado@gmail.com>
2022-04-25 11:16:18 -0300

    Validation

--- index.js
+++ index.js
@@ -13,24 +13,47 @@ const tweets = [];
 
 app.post("/sign-up", (req, res) => {
     const { username, avatar } = req.body;
-    user.username = username;
-    user.avatar = avatar;
-    res.sendStatus(200);
+
+    if (validateUsername(username) && validateAvatarUri(avatar)) {
+        user.username = username;
+        user.avatar = avatar;
+        res.sendStatus(201);
+    } else {
+        res.sendStatus(400);
+    }
 })
 
 app.post("/tweets", (req, res) => {
-
     const { tweet } = req.body;
-    tweets.push({
-        username: user.username,
-        avatar: user.avatar,
-        tweet: tweet
-    });
-    res.sendStatus(200);
+    if (validateTweet(tweet)) {
+        tweets.push({
+            username: user.username,
+            avatar: user.avatar,
+            tweet: tweet
+        });
+        res.sendStatus(201);
+    } else {
+        res.sendStatus(400);
+    }
 })
 
 app.get("/tweets", (req, res) => {
     res.send(tweets);
 })
+
+// Validations
+
+function validateUsername(username) {
+    return (typeof username === 'string' && username !== "");
+}
+
+function validateAvatarUri(avatarUri) {
+    return (avatarUri.match(/\.(jpeg|jpg|gif|png)$/) != null);
+}
+
+function validateTweet(tweet) {
+    return (typeof tweet === 'string' && tweet !== "");
+}
+
 
 app.listen(5000, () => console.log(`Started server at http://localhost:5000!`));
